fd3cd2a07dac0fab02a1362b5337f739e1750e09
keshav <k21keshav05@gmail.com>
2025-09-01 02:52:50 +0530

    Working with data

--- spring-boot-demo/src/main/java/com/example/spring_boot_demo/controllers/UserController.java
+++ spring-boot-demo/src/main/java/com/example/spring_boot_demo/controllers/UserController.java
@@ -4,13 +4,26 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.spring_boot_demo.models.Users;
+
+import java.util.Arrays;
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/users")
 
 public class UserController {
 
+//    public String getUsers(){
+//        return "Hello Keshav";
+//    }
+
     @GetMapping
-    public String getUsers(){
-        return "Hello Keshav";
+    public List<Users> getUsers(){
+        return Arrays.asList(
+                new Users(1L, "Keshav", "k21keshav05@gmail.com"),
+                new Users(2L, "Sam", "Sam@gmail.com"),
+                new Users(3L, "Peter", "peter@gmail.com")
+        );
     }
 }
