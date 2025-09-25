16c965ade6ee817d0835bd0a1249bbe522b3bf2b
keshav <k21keshav05@gmail.com>
2025-09-25 23:01:15 +0530

    Get User List API

--- spring-boot-demo/src/main/java/com/example/spring_boot_demo/controllers/UserController.java
+++ spring-boot-demo/src/main/java/com/example/spring_boot_demo/controllers/UserController.java
@@ -1,5 +1,8 @@
 package com.example.spring_boot_demo.controllers;
 
+import com.example.spring_boot_demo.entity.UserEntity;
+import com.example.spring_boot_demo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,14 +20,17 @@ public class UserController {
 //    public String getUsers(){
 //        return "Hello Keshav";
 //    }
+    @Autowired
+    private UserRepository userRepository;
 
     @GetMapping
-    public List<Users> getUsers(){
-        return Arrays.asList(
-                new Users(1L, "Keshav", "k21keshav05@gmail.com"),
-                new Users(2L, "Sam", "Sam@gmail.com"),
-                new Users(3L, "Peter", "peter@gmail.com")
-        );
+    public List<UserEntity> getUsers(){
+//        return Arrays.asList(
+//                new Users(1L, "Keshav", "k21keshav05@gmail.com"),
+//                new Users(2L, "Sam", "Sam@gmail.com"),
+//                new Users(3L, "Peter", "peter@gmail.com")
+//        );
+        return userRepository.findAll();
     }
 }
 
