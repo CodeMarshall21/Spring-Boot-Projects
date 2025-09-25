900ce3fd15eec96de8edf9b4c456d9d53170c526
keshav <k21keshav05@gmail.com>
2025-09-25 22:51:51 +0530

    Adding getters and setters for UserEntity

--- spring-boot-demo/src/main/java/com/example/spring_boot_demo/entity/UserEntity.java
+++ spring-boot-demo/src/main/java/com/example/spring_boot_demo/entity/UserEntity.java
@@ -23,5 +23,29 @@ public class UserEntity {
         super();
     }
 
+    public Long getId(){
+        return id;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+
+    public void setId(Long id){
+        this.id = id;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setEmail(String email){
+        this.email = email;
+    }
+
 
 }
