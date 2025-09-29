220447a91ca84da9f8a940f38f212d43beb647cb
keshav <k21keshav05@gmail.com>
2025-09-29 22:38:50 +0530

    Added HomeController

---
+++ spring-boot-demo/src/main/java/com/example/spring_boot_demo/controllers/HomeController.java
@@ -0,0 +1,15 @@
+package com.example.spring_boot_demo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody
+public class HomeController {
+
+    @GetMapping("/home")
+    public String getHome(){
+        return "✨ Server is up and running! Welcome ✨";
+    }
+}
