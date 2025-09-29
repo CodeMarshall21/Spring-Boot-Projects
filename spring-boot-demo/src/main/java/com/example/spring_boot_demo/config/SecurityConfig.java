ebcca4536951ab32d9e73262a61fda12bb564f7b
keshav <k21keshav05@gmail.com>
2025-09-30 00:29:07 +0530

    Login With In-Memory Users

--- spring-boot-demo/src/main/java/com/example/spring_boot_demo/config/SecurityConfig.java
+++ spring-boot-demo/src/main/java/com/example/spring_boot_demo/config/SecurityConfig.java
@@ -7,6 +7,8 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -27,17 +29,22 @@ public class SecurityConfig {
     }
 
     @Bean
-    public UserDetailsService userDetailsService(){
+    public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder){
         UserDetails user = User.withUsername("Bro")
-                .password("Bro@123")
+                .password(passwordEncoder.encode("Bro@123"))
                 .roles("USER")
                 .build();
 
         UserDetails admin = User.withUsername("AdminGuy")
-                .password("AdminGuy@123")
+                .password(passwordEncoder.encode("AdminGuy@123"))
                 .roles("ADMIN")
                 .build();
 
         return new InMemoryUserDetailsManager(user, admin);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
