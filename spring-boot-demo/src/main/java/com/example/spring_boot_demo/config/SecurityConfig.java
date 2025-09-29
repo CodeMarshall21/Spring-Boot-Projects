68bc6749fd72f5d38cf2af18c55810a3b36deee7
keshav <k21keshav05@gmail.com>
2025-09-29 22:52:02 +0530

    Creating Security Configuration Class

---
+++ spring-boot-demo/src/main/java/com/example/spring_boot_demo/config/SecurityConfig.java
@@ -0,0 +1,14 @@
+package com.example.spring_boot_demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity){
+        
+    }
+}
