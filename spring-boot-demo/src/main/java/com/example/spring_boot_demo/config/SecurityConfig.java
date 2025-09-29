a6c3fa41216ae6fe23ec7a26ff85c82c2a71662a
keshav <k21keshav05@gmail.com>
2025-09-29 23:48:04 +0530

    Enabling Default Login Page

--- spring-boot-demo/src/main/java/com/example/spring_boot_demo/config/SecurityConfig.java
+++ spring-boot-demo/src/main/java/com/example/spring_boot_demo/config/SecurityConfig.java
@@ -15,7 +15,8 @@ public class SecurityConfig {
 
         httpSecurity.authorizeHttpRequests(authz -> authz.requestMatchers("/api/users/**").authenticated()
                 .requestMatchers("/","/swagger-ui/**").permitAll()
-        );
+        )
+                .formLogin(form -> form.permitAll());
 
         return httpSecurity.build();
     }
