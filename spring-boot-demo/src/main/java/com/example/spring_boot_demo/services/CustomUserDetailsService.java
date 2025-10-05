87a7af1bf2deda5e6d2609f6576ad080327453cb
keshav <k21keshav05@gmail.com>
2025-10-05 23:57:15 +0530

    Creating Custom UserDetailsService

--- spring-boot-demo/src/main/java/com/example/spring_boot_demo/services/CustomUserDetailsService.java
+++ spring-boot-demo/src/main/java/com/example/spring_boot_demo/services/CustomUserDetailsService.java
@@ -3,12 +3,11 @@ package com.example.spring_boot_demo.services;
 import com.example.spring_boot_demo.entity.UserEntity;
 import com.example.spring_boot_demo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
-import javax.swing.text.html.Option;
-import java.util.Optional;
 
 public class CustomUserDetailsService implements UserDetailsService {
 
@@ -18,8 +17,10 @@ public class CustomUserDetailsService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUserName(String username) throws UsernameNotFoundException{
 
-        UserEntity user = userRepository.findByUsername(username).orElseThrow;
+        UserEntity user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User NOT FOUND with that username"));
 
+        return new User(user.getUsername(), user.getPassword(), null);
 
     }
 
